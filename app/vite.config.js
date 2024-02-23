3098e2f35e59d85bcd0e4c9ec107a97778776b58
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2024-02-23 12:36:19 -0300

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, '../src/main.jsx'),
-      },
-    },
   },
 });
